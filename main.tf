62ecd17c48e86f54df10ef2b28fa964a757b7ce7
Tania Kolesnik <kolesnik@gmail.com>
2026-05-16 16:44:09 +0100

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
   }
@@ -20,37 +20,83 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+variable "network_names" {
+  default = ["one", "two", "three", "four"]
+  type    = list(string)
+}
+
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+  for_each = toset(var.network_names)
+
+  name                = "${var.prefix}-${each.key}-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "internal" {
+  for_each = azurerm_virtual_network.main
+
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  virtual_network_name = each.value.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+  for_each = azurerm_subnet.internal
+
+  name                = "${var.prefix}-${each.key}}-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = each.value.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
+variable "security_rules" {
+  default = {
+    "storage1" = { "name" = "one-sg", "priority" = 1001, "direction" = "Inbound", "access" = "Allow", "protocol" = "Tcp", "source_port_range" = "*", "destination_port_range" = "22" },
+    "storage2" = { "name" = "two-sg", "priority" = 1002, "direction" = "Inbound", "access" = "Allow", "protocol" = "Tcp", "source_port_range" = "*", "destination_port_range" = "22" },
+    "storage3" = { "name" = "three-sg", "priority" = 1003, "direction" = "Inbound", "access" = "Allow", "protocol" = "Tcp", "source_port_range" = "*", "destination_port_range" = "22" }
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  dynamic "security_rule" {
+    for_each = var.security_rules
+    content {
+      name                   = security_rule.value.name
+      priority               = security_rule.value.priority
+      direction              = security_rule.value.direction
+      access                 = security_rule.value.access
+      protocol               = security_rule.value.protocol
+      source_port_range      = security_rule.value.source_port_range
+      destination_port_range = security_rule.value.destination_port_range
+    }
+  }
+}
+
+locals {
+  network_interface_ids = [
+    for nic in azurerm_network_interface.main :
+    nic.id
+  ]
+}
+
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
+  count                 = 2
+  name                  = "${var.prefix}-${count.index}-vm"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  network_interface_ids = [local.network_interface_ids[count.index]]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
@@ -76,4 +122,34 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     environment = "staging"
   }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "machine_name" {
+  value = [
+    for vm in azurerm_virtual_machine.main :
+    upper(vm.name)
+  ]
+}
+
+variable "tags" {
+  default = {
+    environment = "dev"
+    owner       = "tania"
+    project     = "terraform"
+  }
+}
+
+output "tags" {
+  value = join(", ", values(var.tags))
+}
+
+output "vm_ids" {
+  value = [
+    for vm in azurerm_virtual_machine.main :
+    vm.id
+  ]
 }
